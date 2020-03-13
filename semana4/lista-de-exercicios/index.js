a5f38473d5659533d400a4bff74c808b255cfb25
joaopfa <joaopfa@hotmail.com>
2020-03-13 17:02:43 -0300

    comitando para fazer a entrega parcial incompleta

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -102,5 +102,20 @@ console.log(menor)
 // e) !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)
 // True
 
+
+// 3. Você tem que escrever um código que, dado um número **N**, ele imprima (no `console`) os **N** 
+// primeiros números pares (por exemplo, se **N** for 3, você deve imprimir 0, 2 e 4; se **N** for 5, deve 
+// imprimir 0, 2, 4, 6 e 8).  Um colega seu disse que já começou esta tarefa, mas não conseguiu terminar. 
+// Dê uma olhada no código dele:
+
+//         const quantidadeDeNumerosPares
+//         let i = 0
+//         while(i <= quantidadeDeNumerosPares) {
+//           console.log(i*2)
+//         }
+
+//     Este código funciona? Por quê? Caso não funcione, corrija a implementação dele.
+
+
 ///////////// FIM EXERCÍCIOS DE LÓGICA DE PROGRAMAÇÃO ////////////////////
 
