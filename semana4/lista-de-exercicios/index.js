4d0653046123b4989157a4e591cdc18af1b70f9d
joaopfa <joaopfa@hotmail.com>
2020-03-13 14:18:14 -0300

    Óbvio que eu ia esquecer de ir commitando os primeiros exercícios rsrs. Aqui vai do 1 ao 3 de interpretação

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,62 @@
+/////////// INÍCIO EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO ////////////////////
+
+//1. O código em questão multiplica a quantidade de dólares inserida no parâmetro da função 
+//(conversorDeMoeda(100)) pela cotação do dólar inserida no prompt, retornando a cifra em 
+//R$ dessa conversão.  O console.log(meuDinheiro) vai imprimir no console, caso o valor inserido
+//no prompt seja 5, e o parâmetro seja = 100:
+// R$ 500
+
+
+//2. O código em questão retorna o valor do montante de um dinheiro investido, para quatro tipos
+//de fundos de investimento. Caso o parâmetro do fundo de investimento não seja um dos tipos de 
+//investimentos que satisfaça a função, o código emite um alerta dizendo que o tipo de investimento
+//informado é incorreto.
+//Com  console.log(novoMontante)  e console.log(segundoMontante), será impresso no console: 
+//  165
+// "TIPO DE INVESTIMENTO INFORMADO INCORRETO"
+
+
+//3. O código em questão percorre o array 'numeros' com a variável 'numero' assumindo cada valor
+//do array, verificando se cada número do array é par. Quando um número do array é par, ele é 
+//adicionado ao array1. Do contrário, ele é adicionado ao array 2. Portanto o código basicamente
+//separa o array original em dois novos arrays, um com os números pares, e outro com os números
+//ímpares do array original.
+//Os console.log 's vão imprimir no console:
+
+//console.log("Quantidade total de números", numeros.length) ---> 
+//   Quantidade total de números14
+//console.log(array1.length) ---> 6
+//console.log(array2.length) ---> 8
+
+//4. 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////// FIM EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO ////////////////////
