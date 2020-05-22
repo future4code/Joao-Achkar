b2f21972d7e6dff3e0ad95617d50e56ff7c2a6a2
joaopfa <joaopfa@hotmail.com>
2020-05-22 19:03:45 -0300

    criando contas e passando elas como objetos para um array no newAcc.json

--- semana14/projetF4Bank/CreateAccount/src/createAcc.ts
+++ semana14/projetF4Bank/CreateAccount/src/createAcc.ts
@@ -1,11 +1,43 @@
 import * as fs from 'fs';
+type user = {
+    name: string,
+    cpf: number,
+    birthDate: number,
+    balance: number,
+};
 
-const listFileName: string = process.argv [2]
-const newTask: string = process.argv [3]
+let accounts: any = []
 
-try {
-    fs.appendFileSync(listFileName, `\n${newTask}`);
+const fileName: string = (process.argv[2])
+const userName: string = (process.argv[3])
+const cpf: number = Number(process.argv[4])
+const birthDate: number = Number(process.argv[5])
+const balance: number = Number(process.argv[6])
+
+const account: user = {
+    name: userName,
+    cpf: cpf,
+    birthDate: birthDate,
+    balance: balance
+}
+
+function createAccount(newAccount: user): void {
+try { 
+    const data: Buffer = fs.readFileSync(fileName);
+    accounts = JSON.parse(data.toString());
+    accounts.push(account)
+    fs.writeFileSync(fileName, JSON.stringify(accounts, null, 2));
     console.log("Conta criada com sucesso")
- } catch(err){
-     console.error(err)
- };
+} catch (err) {
+    console.error(err)
+}
+}
+
+createAccount(account)
+
+// type extractArray = {
+//     value: number,
+//     date: number,
+//     description: string,
+// }
+
